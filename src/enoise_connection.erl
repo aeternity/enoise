64d1a048a5e8d7830e4197aee8c4de94e9a80549
Hans Svensson <hans.svensson@quviq.com>
2018-05-02 20:50:26 +0200

    no lager debug in send

--- src/enoise_connection.erl
+++ src/enoise_connection.erl
@@ -160,8 +160,7 @@ handle_msgs(S = #state{ msgbuf = [Msg | Msgs], active = {once, Delivered}, owner
 
 handle_send(S = #state{ tcp_sock = TcpSock, tx = Tx }, Data) ->
     {ok, Tx1, Msg} = enoise_cipher_state:encrypt_with_ad(Tx, <<>>, Data),
-    Res = gen_tcp:send(TcpSock, <<(byte_size(Msg)):16, Msg/binary>>),
-    lager:debug("~p sent ~p bytes :: ~p", [self(), byte_size(Data), Res]),
+    ok = gen_tcp:send(TcpSock, <<(byte_size(Msg)):16, Msg/binary>>),
     {ok, S#state{ tx = Tx1 }}.
 
 set_active(#state{ msgbuf = [], active = {once, _}, tcp_sock = TcpSock }) ->
