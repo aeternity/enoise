6de4d0bf71a6a652f6b15314eee01e591a77c9dc
Hans Svensson <hans.svensson@quviq.com>
2018-12-21 09:34:24 +0100

    Add (and refactor) tests for bad data in handshake

---
+++ test/enoise_bad_data_tests.erl
@@ -0,0 +1,27 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2018, Aeternity Anstalt
+%%%-------------------------------------------------------------------
+-module(enoise_bad_data_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+bad_data_hs_1_test() ->
+    SrvKeyPair = enoise_keypair:new(dh25519),
+    Proto      = enoise_protocol:to_name(xk, dh25519, 'ChaChaPoly', blake2b),
+    Opts       = [{echos, 1}, {reply, self()}],
+    Srv        = enoise_utils:echo_srv_start(4567, Proto, SrvKeyPair, Opts),
+
+    bad_client(4567),
+
+    SrvRes =
+        receive {Srv, server_result, Res0} -> Res0
+        after 500 -> timeout end,
+    ?assertMatch({error, {bad_data, _}}, SrvRes),
+    ok.
+
+bad_client(Port) ->
+    {ok, Sock} = gen_tcp:connect("localhost", Port, [binary, {reuseaddr, true}], 100),
+    gen_tcp:send(Sock, <<0:256/unit:8>>),
+    timer:sleep(100),
+    gen_tcp:close(Sock).
+
